fae24ba67b5c4a3a4d9ab853f6ba1f7e40774561
Steffen Klassert <steffen.klassert@secunet.com>
2015-09-22 10:37:10 +0200

    esp: Add a software GRO codepath
    This patch adds GRO callbacks for ESP on ipv4 and ipv6.
    
    In case the GRO layer detects an ESP packet, the
    esp4_gro_receive() function calls the xfrm input layer
    which decapsulates the packet and reinject it into
    layer 2 by calling netif_rx(). We use on bit of the
    sk_buff to flag xfrm_gro. If this bit is set, the
    process_backlog() function calls napi_gro_receive()
    intead of __netif_receive_skb(). We could avoid the
    usage of xfrm_gro if we could call napi_gro_receive()
    unconditionaly from process_backlog().
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4453,6 +4453,11 @@ static enum gro_result dev_gro_receive(struct napi_struct *napi, struct sk_buff 
 	}
 	rcu_read_unlock();
 
+	if (PTR_ERR(pp) == -EINPROGRESS) {
+		ret = GRO_CONSUMED;
+		goto ok;
+	}
+
 	if (&ptype->list == head)
 		goto normal;
 
@@ -4559,6 +4564,7 @@ static gro_result_t napi_skb_finish(gro_result_t ret, struct sk_buff *skb)
 
 	case GRO_HELD:
 	case GRO_MERGED:
+	case GRO_CONSUMED:
 		break;
 	}
 
@@ -4629,6 +4635,7 @@ static gro_result_t napi_frags_finish(struct napi_struct *napi,
 		break;
 
 	case GRO_MERGED:
+	case GRO_CONSUMED:
 		break;
 	}
 
@@ -4770,7 +4777,12 @@ static int process_backlog(struct napi_struct *napi, int quota)
 		while ((skb = __skb_dequeue(&sd->process_queue))) {
 			rcu_read_lock();
 			local_irq_enable();
-			__netif_receive_skb(skb);
+
+			if (skb->xfrm_gro)
+				napi_gro_receive(napi, skb);
+			else
+				__netif_receive_skb(skb);
+
 			rcu_read_unlock();
 			local_irq_disable();
 			input_queue_head_incr(sd);
