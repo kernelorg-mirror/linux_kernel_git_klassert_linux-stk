312c43b802fe65101dd5360158fffc0fc2941a2f
Steffen Klassert <steffen.klassert@secunet.com>
2016-10-25 09:29:26 +0200

    esp: Use a synchronous crypto algorithm on offloading.
    We need a fallback algorithm for crypto offloading to a NIC.
    This is because packets can be rerouted to other NICs that
    don't support crypto offloading. The fallback is going to be
    implemented at layer2 where we know the final output device
    but can't handle asynchronous returns fron the crypto layer.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/ipv6/esp6.c
+++ net/ipv6/esp6.c
@@ -708,13 +708,17 @@ static int esp_init_aead(struct xfrm_state *x)
 	char aead_name[CRYPTO_MAX_ALG_NAME];
 	struct crypto_aead *aead;
 	int err;
+	u32 mask = 0;
 
 	err = -ENAMETOOLONG;
 	if (snprintf(aead_name, CRYPTO_MAX_ALG_NAME, "%s(%s)",
 		     x->geniv, x->aead->alg_name) >= CRYPTO_MAX_ALG_NAME)
 		goto error;
 
-	aead = crypto_alloc_aead(aead_name, 0, 0);
+	if (x->xso.offload_handle)
+		mask |= CRYPTO_ALG_ASYNC;
+
+	aead = crypto_alloc_aead(aead_name, 0, mask);
 	err = PTR_ERR(aead);
 	if (IS_ERR(aead))
 		goto error;
@@ -744,6 +748,7 @@ static int esp_init_authenc(struct xfrm_state *x)
 	char authenc_name[CRYPTO_MAX_ALG_NAME];
 	unsigned int keylen;
 	int err;
+	u32 mask = 0;
 
 	err = -EINVAL;
 	if (!x->ealg)
@@ -769,7 +774,10 @@ static int esp_init_authenc(struct xfrm_state *x)
 			goto error;
 	}
 
-	aead = crypto_alloc_aead(authenc_name, 0, 0);
+	if (x->xso.offload_handle)
+		mask |= CRYPTO_ALG_ASYNC;
+
+	aead = crypto_alloc_aead(authenc_name, 0, mask);
 	err = PTR_ERR(aead);
 	if (IS_ERR(aead))
 		goto error;
